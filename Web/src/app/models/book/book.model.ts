bda976fa2040b22cfdc46c72ac4d45b0fa1efdc1
mukhsitht <tmukhsith256@yahoo.com>
2024-11-13 04:12:27 +0300

    updates

--- Web/src/app/models/book/book.model.ts
+++
@@ -1,9 +0,0 @@
-export class Book {
-  "id": number;
-  "title": string;
-  "author": string;
-  "price": number;
-  "formattedPrice": string;
-  "publicationDate": Date;
-  "formattedPublicationDate": string;
-}
--- Web/src/app/models/book/book.search.model.ts
+++
@@ -1,7 +0,0 @@
-export class SearchBook {
-  "title": string;
-  "author": string;
-  "price": string;
-  "date": string;
-  "sortingId": string;
-}
